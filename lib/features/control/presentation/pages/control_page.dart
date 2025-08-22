9361d8814ab6a8008ef8401445adc898b483f4a5
daniel-m <danieldfgro2000@gmail.com>
2025-08-23 01:41:41 +0300

    control page ok

--- lib/features/control/presentation/pages/control_page.dart
+++ lib/features/control/presentation/pages/control_page.dart
@@ -41,183 +41,141 @@ class _ControlPageState extends State<ControlPage>
   Widget build(BuildContext context) {
     final controlBloc = context.read<ControlBloc>();
 
-    return OrientationBuilder(
-      builder: (context, orientation) {
-        final isPortrait = orientation == Orientation.portrait;
-
-        return Scaffold(
-          appBar: isPortrait
-              ? AppBar(
-                  title: const Text('Mower Control'),
-                  centerTitle: true,
-                  backgroundColor: isRecording ? Colors.orange : null,
-                )
-              : null,
-          body: LayoutBuilder(
-            builder: (context, constraints) {
-              return Stack(
-                fit: StackFit.expand,
-                children: [
-                  EspMjpegView(),
-                  Positioned.fill(
-                    top: 0,
-                    left: 0,
-                    child: SafeArea(
-                      child: Padding(
-                        padding: const EdgeInsets.all( 16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            if (isRecording)
-                              Container(
-                                width: double.infinity,
-                                color: Colors.orange,
-                                padding: const EdgeInsets.all(8.0),
-                                child: const Text(
-                                  textScaler: TextScaler.linear(0.8),
-                                  'Recording in progress...',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 100.0,
-                                  width: 100.0,
-                                  child: Center(
-                                    child: Joystick(
-                                      mode: JoystickMode.horizontal,
-                                      listener: (details) {
-                                        steering = details.x * 30;
-                                        controlBloc.add(
-                                          DriveCommand(
-                                            steering: steering,
-                                            isMoving: isMoving,
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(width: 10.0),
-                                IntrinsicWidth(
-                                  child: SwitchListTile(
-                                    activeColor: Colors.green,
-                                    title: const Text(
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        textScaler: TextScaler.linear(0.8),
-                                        'Move forward'),
-                                    subtitle: const Text(
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        textScaler: TextScaler.linear(0.6),
-                                        'Engage mower drive'),
-                                    value: isMoving,
-                                    dense: true,
-                                    shape: RoundedRectangleBorder(
-                                      side: BorderSide(
-                                        color: Theme.of(context).colorScheme.primary,
-                                      ),
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    onChanged: (value) {
-                                      setState(() => isMoving = value);
-                                      controlBloc.add(
-                                        DriveCommand(
-                                          steering: steering,
-                                          isMoving: isMoving,
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                                const SizedBox(width: 10.0),
-                                IntrinsicWidth(
-                                  child: SwitchListTile(
-                                    activeColor: Colors.red,
-                                    title: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        const Text(
-                                            maxLines: 1,
-                                            overflow: TextOverflow.ellipsis,
-                                            textScaler: TextScaler.linear(0.8),
-                                            'Record Path'),
-                                        const SizedBox(width: 10.0),
-                                        if (isRecording)
-                                          FadeTransition(
-                                            opacity: _blinkController,
-                                            child: const Icon(
-                                              Icons.fiber_manual_record,
-                                              color: Colors.red,
-                                            ),
-                                          ),
-                                      ],
-                                    ),
-                                    subtitle: const Text(
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      textScaler: TextScaler.linear(0.6),
-                                      'Start/stop',
-                                    ),
-                                    value: isRecording,
-                                    dense: true,
-                                    shape: RoundedRectangleBorder(
-                                      side: BorderSide(
-                                        color: Theme.of(context).colorScheme.primary,
-                                      ),
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    onChanged: (value) async {
-                                      setState(() => isRecording = value);
-                                      value
-                                          ? controlBloc.add(StartRecord())
-                                          : controlBloc.add(
-                                        StopRecord(
-                                          fileName:
-                                          await _askPathName(context) ??
-                                              'path_${DateTime.now().millisecondsSinceEpoch}.txt',
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                                const SizedBox(width: 10.0),
-                                ElevatedButton.icon(
-                                  icon: const Icon(Icons.dangerous, size: 36.0),
-                                  onPressed: () => controlBloc.add(EmergencyStop()),
-                                  label: const Text('STOP'),
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.red,
-                                    foregroundColor: Colors.white,
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 24.0,
-                                      horizontal: 24.0,
-                                    ),
-                                  ),
-                                ),
-                              ],
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Stack(
+            children: [
+              EspMjpegView(),
+              if (isRecording) _recordingBanner(context),
+              _recordButton(controlBloc, context),
+              Positioned.fill(
+                top: 0,
+                left: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Spacer(),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          _driveUnit(controlBloc),
+                          Spacer(),
+                          IconButton(
+                            iconSize: 120.0,
+                            splashColor: Colors.red.shade500,
+                            icon: Icon(
+                              Icons.stop_circle_rounded,
+                              color: isMoving ? Colors.red : Colors.grey,
                             ),
-                          ],
-                        ),
+                            onPressed: () {
+                              setState(() => isMoving = false);
+                              controlBloc.add(EmergencyStop());
+                            },
+                          ),
+                        ],
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              );
-            }
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Column _driveUnit(ControlBloc controlBloc) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          iconSize: 72.0,
+          icon: Icon(
+            color: isMoving ? Colors.green : Colors.grey,
+            Icons.keyboard_double_arrow_up_sharp,
           ),
-        );
-      },
+          onPressed: () {
+            setState(() => isMoving = !isMoving);
+            controlBloc.add(
+              DriveCommand(steering: steering, isMoving: isMoving),
+            );
+          },
+        ),
+        SizedBox(
+          height: 100.0,
+          width: 100.0,
+          child: Center(
+            child: Joystick(
+              mode: JoystickMode.horizontal,
+              listener: (details) {
+                steering = details.x * 30;
+                controlBloc.add(
+                  DriveCommand(steering: steering, isMoving: isMoving),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Positioned _recordButton(ControlBloc controlBloc, BuildContext context) {
+    return Positioned(
+      top: 40,
+      right: 20,
+      child: IconButton(
+        tooltip: "Record Path",
+        isSelected: isRecording,
+        onPressed: () async => isRecording
+            ? {
+                setState(() => isRecording = false),
+                controlBloc.add(
+                  StopRecord(
+                    fileName:
+                        await _askPathName(context) ??
+                        'path_${DateTime.now().millisecondsSinceEpoch}.txt',
+                  ),
+                ),
+              }
+            : {
+                setState(() => isRecording = true),
+                controlBloc.add(StartRecord()),
+              },
+        icon: isRecording
+            ? FadeTransition(
+                opacity: _blinkController,
+                child: const Icon(Icons.fiber_manual_record, color: Colors.red),
+              )
+            : const Icon(Icons.fiber_manual_record, color: Colors.grey),
+        iconSize: 48.0,
+      ),
+    );
+  }
+
+  Container _recordingBanner(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100.0,
+      color: Colors.orange.shade500,
+      padding: const EdgeInsets.only(top: 50.0, bottom: 10.0),
+      child: Text(
+        textAlign: TextAlign.center,
+        textScaler: TextScaler.linear(1.1),
+        'Recording PATH in progress...',
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.onPrimary,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 
